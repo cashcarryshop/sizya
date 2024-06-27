94b65b664426ff13a0365b18419f4e0173676359
TheWhatis <anton-gogo@mail.ru>
2024-06-27 14:06:39 +0700

    Создал потоки, которые могут кодировать или декодировать в gzip

---
+++ src/Http/Io/InflateStream.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Поток, с возможностью декодирования gzip
+ *
+ * PHP version 8
+ *
+ * @category Http
+ * @package  Sizya
+ * @author   TheWhatis <anton-gogo@mail.ru>
+ * @license  Unlicense <https://unlicense.org>
+ * @link     https://github.com/cashcarryshop/sizya
+ */
+
+namespace CashCarryShop\Sizya\Http\Io;
+
+use GuzzleHttp\Psr7\Stream;
+use GuzzleHttp\Psr7\StreamDecoratorTrait;
+use GuzzleHttp\Psr7\StreamWrapper;
+use Psr\Http\Message\StreamInterface;
+
+/**
+ * Поток, с возможностью декодирования gzip
+ *
+ * @category Http
+ * @package  Sizya
+ * @author   TheWhatis <anton-gogo@mail.ru>
+ * @license  Unlicense <https://unlicense.org>
+ * @link     https://github.com/cashcarryshop/sizya
+ */
+final class InflateStream implements StreamInterface
+{
+    use StreamDecoratorTrait;
+
+    /**
+     * Поток
+     *
+     * @var StreamInterface
+     */
+    private StreamInterface $stream;
+
+    /**
+     * Создание экземпляра закодированного потока
+     *
+     * @param StreamInterface $stream Поток
+     * @param array           $params Параметры
+     */
+    public function __construct(StreamInterface $stream, array $params = [])
+    {
+        $resource = StreamWrapper::getResource($stream);
+        stream_filter_append($resource, 'zlib.inflate', STREAM_FILTER_READ, $params);
+        $this->stream = $stream->isSeekable()
+            ? new Stream($resource)
+            : new NoSeekStream(new Stream($resource));
+    }
+}
