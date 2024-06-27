05db6854a859e52c43124fdecc936b204e9d85f3
TheWhatis <anton-gogo@mail.ru>
2024-06-27 14:09:37 +0700

    Создал PoolInterface для конкуретного отправления запросов

---
+++ src/Http/PoolInterface.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Интерфейс Pool (бассейна) для запросов
+ *
+ * PHP version 8
+ *
+ * @category Http
+ * @package  Sizya
+ * @author   TheWhatis <anton-gogo@mail.ru>
+ * @license  Unlicense <https://unlicense.org>
+ * @link     https://github.com/cashcarryshop/sizya
+ */
+
+namespace CashCarryShop\Sizya\Http;
+
+use Psr\Http\Message\RequestInterface;
+use GuzzleHttp\Promise\PromiseInterface;
+use GuzzleHttp\Promise\PromisorInterface;
+
+/**
+ * Интерфейс Pool (бассейна) для запросов
+ *
+ * @category Http
+ * @package  Sizya
+ * @author   TheWhatis <anton-gogo@mail.ru>
+ * @license  Unlicense <https://unlicense.org>
+ * @link     https://github.com/cashcarryshop/sizya
+ */
+interface PoolInterface extends PromisorInterface
+{
+    /**
+     * Создать экземпляр Pool
+     *
+     * @param SenderInterface            $sender   Отправитель запросов
+     * @param iterable<RequestInterface> $requests Запросы
+     * @param int                        $limit    Ограничение
+     */
+    public function __construct(
+        SenderInterface $sender,
+        iterable $requests,
+        int $limit
+    );
+
+    /**
+     * Получить все полученные Promise из Poool
+     *
+     * @return iterable<PromiseInterface>
+     */
+    public function getPromises(): iterable;
+}
