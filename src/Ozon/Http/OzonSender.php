066f183c467d489ec07fe31f80afdbdc7844a565
TheWhatis <anton-gogo@mail.ru>
2024-06-27 14:13:04 +0700

    Создал реализации Sender для каждого сервиса, и обработчики для них

---
+++ src/Ozon/Http/OzonSender.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Класс отправителя для Ozon
+ *
+ * PHP version 8
+ *
+ * @category Ozon
+ * @package  Sizya
+ * @author   TheWhatis <anton-gogo@mail.ru>
+ * @license  Unlicense <https://unlicense.org>
+ * @link     https://github.com/cashcarryshop/sizya
+ */
+
+namespace CashCarryShop\Sizya\Ozon\Http;
+
+use CashCarryShop\Sizya\Http\Sender;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+
+/**
+ * Класс отправителя для Ozon
+ *
+ * @category Ozon
+ * @package  Sizya
+ * @author   TheWhatis <anton-gogo@mail.ru>
+ * @license  Unlicense <https://unlicense.org>
+ * @link     https://github.com/cashcarryshop/sizya
+ */
+class OzonSender extends Sender
+{
+    /**
+     * Создание экземпляра отправителя
+     *
+     * @param ?ClientInterface $client Клиент
+     */
+    public function __construct(?ClientInterface $client = null)
+    {
+        if (null === $client) {
+            $stack = HandlerStack::create();
+            $stack->push(PrepareBodyMiddleware::create());
+            $client = new Client(['handler' => $stack]);
+        }
+
+        parent::__construct($client);
+    }
+
+}
