8b8ebcf2f179f7080cc8be42ee494520fb28b18b
TheWhatis <anton-gogo@mail.ru>
2024-07-12 19:48:43 +0700

    Добавил вспомогательный метод для работы с форматами даты МойСклад

--- src/Moysklad/Utils.php
+++ src/Moysklad/Utils.php
@@ -17,6 +17,7 @@ namespace CashCarryShop\Sizya\Moysklad;
 use CashCarryShop\Sizya\Http\Utils as HttpUtils;
 use GuzzleHttp\Promise\PromiseInterface;
 use Psr\Http\Message\ResponseInterface;
+use DateTimeZone;
 
 /**
  * Перечисление доступных знаков сравнения
@@ -44,5 +45,24 @@ class Utils extends HttpUtils
         }
 
         return $value ?? '';
+    }
+
+    /**
+     * Конвертировать дату и время в формат для МойСклад
+     *
+     * @param string        $format   Формат
+     * @param string        $datetime Дата и время
+     * @param ?DateTimeZone $timezone Часовой пояс времени
+     *
+     * @return string
+     */
+    public static function createFromFormat(
+        string $format,
+        string $datetime,
+        ?DateTimeZone $timezone
+    ): string {
+        return date_create_from_format($format, $datetime, $timezone)
+            ->setTimezone(new DateTimeZone('Europe/Moscow'))
+            ->format('Y-m-d H:i:s');
     }
 }
