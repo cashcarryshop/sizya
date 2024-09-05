14792413a25d685fa39699c415b25488edb031dc
TheWhatis <anton-gogo@mail.ru>
2024-09-05 22:29:02 +0700

    Создал dto для обновления остатков

---
+++ src/DTO/StockUpdateDTO.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * DTO для обновления остатков
+ *
+ * PHP version 8
+ *
+ * @category Stocks
+ * @package  Sizya
+ * @author   TheWhatis <anton-gogo@mail.ru>
+ * @license  Unlicense <https://unlicense.org>
+ * @link     https://github.com/cashcarryshop/sizya
+ */
+
+namespace CashCarryShop\Sizya\DTO;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * DTO для обновления остатков
+ *
+ * @category Stocks
+ * @package  Sizya
+ * @author   TheWhatis <anton-gogo@mail.ru>
+ * @license  Unlicense <https://unlicense.org>
+ * @link     https://github.com/cashcarryshop/sizya
+ *
+ * Поля `id` и `article` взаимозаменяемые,
+ * но обязательные
+ *
+ * @property string  $id          Идентификатор позиции
+ * @property string  $article     Артикул товара
+ * @property string  $warehouseId Идентификатор склада
+ * @property int     $quantity    Количество товаров
+ */
+class StockUpdateDTO extends AbstractDTO
+{
+    /**
+     * Идентификатор позиции
+     *
+     * @var ?string
+     */
+    #[Assert\When(
+        expression: '$this->article === null',
+        constraints: [new Assert\NotBlank]
+    )]
+    public readonly ?string $id;
+
+    /**
+     * Артикул товара
+     *
+     * @var ?string
+     */
+    #[Assert\When(
+        expression: '$this->id === null',
+        constraints: [new Assert\NotBlank]
+    )]
+    public readonly ?string $article;
+
+    /**
+     * Идентификатор склада
+     *
+     * @var string
+     */
+    #[Assert\NotBlank]
+    public readonly string $warehouseId;
+
+    /**
+     * Количество товара
+     *
+     * @var int
+     */
+    #[Assert\PositiveOrZero]
+    public readonly int $quantity;
+
+    /**
+     * Создать экземпляр позиции
+     *
+     * @param string  $id          Идентификатор товарв
+     * @param string  $article     Артикул товара
+     * @param string  $warehouseId Идентификатор склада
+     * @param int     $quantity    Количество товаров
+     */
+    public function __construct(
+        string  $id,
+        string  $article,
+        string  $warehouseId,
+        int     $quantity = 0,
+    ) {
+        $this->id          = $id;
+        $this->article     = $article;
+        $this->warehouseId = $warehouseId;
+        $this->quantity    = $quantity;
+    }
+}
