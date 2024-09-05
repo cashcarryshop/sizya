84071cc54d35205a0fc4060a7b995460ba35a441
TheWhatis <anton-gogo@mail.ru>
2024-09-05 21:32:03 +0700

    Создал dto для создания доп. полей

---
+++ src/DTO/CreateAdditionalDTO.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * DTO для дополнительных полей
+ *
+ * PHP version 8
+ *
+ * @category DTO
+ * @package  Sizya
+ * @author   TheWhatis <anton-gogo@mail.ru>
+ * @license  Unlicense <https://unlicense.org>
+ * @link     https://github.com/cashcarryshop/sizya
+ */
+
+namespace CashCarryShop\Sizya\DTO;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * DTO для дополнительных полей
+ *
+ * @property string $entityId Идентификатор сущности доп. поля
+ * @property mixed  $value    Значение доп поля
+ *
+ * @category DTO
+ * @package  Sizya
+ * @author   TheWhatis <anton-gogo@mail.ru>
+ * @license  Unlicense <https://unlicense.org>
+ * @link     https://github.com/cashcarryshop/sizya
+ */
+class CreateAdditionalDTO extends AbstractDTO
+{
+    /**
+     * Идентификатор доп. поля (для создания, тип доп. поля)
+     *
+     * @var string
+     */
+    #[Assert\NotBlank]
+    public readonly string $entityId;
+
+    /**
+     * Значение
+     *
+     * @var mixed
+     */
+    public readonly mixed $value;
+
+    /**
+     * Создать экземпляр доп. поля
+     *
+     * @param string  $entityId Идентификатор сущности доп. поля
+     * @param mixed   $value    Значение доп поля
+     */
+    public function __construct(string $entityId, mixed $value) {
+        $this->entityId = $entityId;
+        $this->value    = $value;
+    }
+}
