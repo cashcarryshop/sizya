de85e6ff220d6bfce01580faeff44598aa6c60f1
TheWhatis <anton-gogo@mail.ru>
2024-09-05 21:25:08 +0700

    Создал интерфейс для dto

---
+++ src/DTO/DTOInterface.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Интерфейс объекта передачи данных
+ *
+ * PHP version 8
+ *
+ * @category DTO
+ * @package  Sizya
+ * @author   TheWhatis <anton-gogo@mail.ru>
+ * @license  Unlicense <https://unlicense.org>
+ * @link     https://github.com/cashcarryshop/sizya
+ */
+
+namespace CashCarryShop\Sizya\DTO;
+
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+
+/**
+ * Интерфейс объекта передачи данных
+ *
+ * @category DTO
+ * @package  Sizya
+ * @author   TheWhatis <anton-gogo@mail.ru>
+ * @license  Unlicense <https://unlicense.org>
+ * @link     https://github.com/cashcarryshop/sizya
+ */
+interface DTOInterface
+{
+    /**
+     * Создать DTO
+     *
+     * @param array $data Данные для создания DTO
+     *
+     * @return static
+     */
+    public static function create(array $data): static;
+
+    /**
+     * Конвертировать в массив
+     *
+     * @return array
+     */
+    public function toArray(): array;
+
+    /**
+     * Конвертировать в json
+     *
+     * @return string
+     */
+    public function toJson(): string;
+
+    /**
+     * Валидировать данные
+     *
+     * @return void
+     * @throw  ValidationFailedException
+     */
+    public function validate(): void;
+}
