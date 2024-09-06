cb6535ff88ec869e2f3d599719bc0d4f8d2a168b
TheWhatis <anton-gogo@mail.ru>
2024-09-06 16:14:08 +0700

    Изменил категорию интерфейсов для работы с остатками

--- src/StocksUpdaterInterface.php
+++ src/StocksUpdaterInterface.php
@@ -4,7 +4,7 @@
  *
  * PHP version 8
  *
- * @category Sizya
+ * @category Stocks
  * @package  Sizya
  * @author   TheWhatis <anton-gogo@mail.ru>
  * @license  Unlicense <https://unlicense.org>
@@ -16,7 +16,7 @@ namespace CashCarryShop\Sizya;
 /**
  * Интерфейс с методами для обновления остатков
  *
- * @category Sizya
+ * @category Stocks
  * @package  Sizya
  * @author   TheWhatis <anton-gogo@mail.ru>
  * @license  Unlicense <https://unlicense.org>
