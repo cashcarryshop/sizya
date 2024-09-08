10632491c3a1ec9c5a1196485277a1c64a7015fc
TheWhatis <anton-gogo@mail.ru>
2024-09-08 21:55:19 +0700

    Переделал работу с ErrorDTO, убрал ReasonDTO и расширил docblock

--- src/DTO/ErrorDTO.php
+++ src/DTO/ErrorDTO.php
@@ -15,9 +15,23 @@ declare(strict_types=1);
 namespace CashCarryShop\Sizya\DTO;
 
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Psr\Http\Message\ResponseInterface;
+use Throwable;
 
 /**
  * DTO Ошибки
+ *
+ * При использовании этого DTO нужно учитывать, что
+ * $reason (причина ошибки) должна быть соответствнно
+ * типу ошибки:
+ *
+ * - Если ошибка возникла в ходе валидации полученных данных,
+ *   то $reason должен быть `ConstraintViolationListInterface`.
+ * - Если ошибка возникла в ходе обращения к API,
+ *   то $reason должен быть `ResponseInterface`.
+ * - Если возникла непредвиденная ошибка, приводящая
+ *   к выводу исключения, то $reason должен быть `Throwable`.
  *
  * @category Orders
  * @package  Sizya
@@ -25,8 +39,8 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @license  Unlicense <https://unlicense.org>
  * @link     https://github.com/cashcarryshop/sizya
  *
- * @property mixed      $value  По какому значению возникла ошибка
- * @property ReasonDTO  $reason Причина ошибки
+ * @property mixed $value  По какому значению возникла ошибка
+ * @property mixed $reason Причина ошибки (тип зависит от правил валидации в DTO)
  *
  * @see ReasonDTO
  */
@@ -35,15 +49,18 @@ class ErrorDTO extends AbstractDTO
     /**
      * Создать экземпляр ошибки
      *
-     * @param mixed     $value  По какому значению возникла ошибка
-     * @param ReasonDTO $reason Причина ошибка
+     * @param mixed $value  По какому значению возникла ошибка
+     * @param mixed $reason Причина ошибка (тип зависит от правил валидации в DTO)
      */
     public function __construct(
         public $value = null,
 
-        #[Assert\Type(ReasonDTO::class)]
         #[Assert\NotBlank]
-        #[Assert\Valid]
+        #[Assert\Type([
+            ConstraintViolationListInterface::class,
+            ResponseInterface::class,
+            Throwable::class
+        ])]
         public $reason = null
     ) {}
 }
