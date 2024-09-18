f30fec8138c28ed29458deddda6041fdc5e82f97
TheWhatis <anton-gogo@mail.ru>
2024-09-18 17:39:21 +0700

    Изменил контракт для создания заказов

--- src/OrdersCreatorInterface.php
+++ src/OrdersCreatorInterface.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Интерфейс с методами для создания заказов
+ * Этот файл является частью пакета sizya
  *
  * PHP version 8
  *
@@ -12,6 +12,10 @@
  */
 
 namespace CashCarryShop\Sizya;
+
+use CashCarryShop\Sizya\DTO\OrderDTO;
+use CashCarryShop\Sizya\DTO\OrderCreateDTO;
+use CashCarryShop\Sizya\DTO\ByErrorDTO;
 
 /**
  * Интерфейс с методами для создания заказов
@@ -27,71 +31,29 @@ interface OrdersCreatorInterface
     /**
      * Массово создать заказы
      *
-     * Все даты должны быть в формате UTC `Y-m-d\TH:i:s\Z`
+     * Количество возвращаемых элементов должно
+     * соответствовать переданным.
      *
-     * Массив $orders должен быть:
+     * @param OrderCreateDTO[] $orders Заказы для создания
      *
-     * - optional(article):       (string)  Артикул заказа
-     * - optional(created):       (string)  Дата создания заказа
-     * - optional(status):        (?string) Статус заказа
-     * - optional(shipment_date): (?string) Планируемая дата отгрузки
-     * - optional(delivery_date): (?string) Планируемая дата доставки
-     * - optional(description):   (?string) Описание
-     * - optional(additional)     (array)   Доп. данные
-     * - optional(positions):     (array)   Массив с позициями
+     * @see OrderCreateDTO
+     * @see OrderDTO
+     * @see ByErrorDTO
      *
-     * Позиции `positions`:
-     *
-     * `orderId` и `article` взаимозаменяемые, обязательные.
-     *
-     * - orderId|article:    (string) Идентификатор или артикул товара
-     * - optional(type)      (string) Тип товара
-     * - optional(quantity): (int)    Количество товаров
-     * - optional(reserve):  (int)    Количество зарезервированных товаров
-     * - optional(price):    (float)  Цена товара
-     * - optional(discount): (float)  Скидка
-     * - optional(currency): (string) Валюта
-     * - optional(vat):      (bool)   Учитывать ли НДС
-     *
-     * Должен возвращать массив:
-     *
-     * Поля id, article, created, status, shipment_date,
-     * delivery_date, additional, positions могут быть не
-     * возвращены, если во время обновления возникла ошибка.
-     *
-     * - optional(id):            (string) Идентификатор заказа
-     * - optional(article):       (string) Артикул заказа
-     * - optional(created):       (string) Дата создания заказа
-     * - optional(status):        (string) Статус заказа
-     * - optional(shipment_date): (string) Планируемая дата отгрузки
-     * - optional(delivery_date): (string) Планируемая дата доставки
-     * - optional(description):   (string) Описание
-     * - optional(additional)     (array)  Доп. данные
-     * - optional(positions):     (array)  Массив с позициями
-     * - error:                   (bool)   Возникла ли ошибка во время обновления
-     * - optional(reason):        (mixed)  Если возникла ошибка, добавляется это поле
-     * - original:                (mixed)  Оригинальный ответ
-     *
-     * Возвращаемый массив должен быть заполнен ровно
-     * на то количество элементов, которые были
-     * переданы в функцию и возвращаться в той-же
-     * последовательности.
-     *
-     * @param array $orders Заказы
-     *
-     * @return array<array>
+     * @return array<int, OrderDTO|ByErrorDTO>
      */
     public function massCreateOrders(array $orders): array;
 
     /**
      * Создать заказ
      *
-     * Должен возвращать данные с созданным заказом,
-     * смотреть `OrdersCreatorInterface::massCreateOrders`.
+     * @param OrderCreateDTO $order Заказ
      *
-     * @param array $order Заказ
+     * @see OrderCreateDTO
+     * @see OrderDTO
+     * @see ByErrorDTO
      *
-     * @return array
+     * @return OrderDTO|ByErrorDTO
      */
-    public function createOrder(array $order): array;
+    public function createOrder(OrderCreateDTO $order): OrderDTO|ByErrorDTO;
 }
