8621d9695ae05cd7b0626a7407463c6ae1b71f95
TheWhatis <anton-gogo@mail.ru>
2024-09-23 20:32:11 +0700

    Написал трейт с тестами методов StocksGetterInterface

---
+++ tests/Traits/StocksGetterTests.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+/**
+ * Этот файл является частью пакета sizya.
+ *
+ * PHP version 8
+ *
+ * @category TestTraits
+ * @package  Sizya
+ * @author   TheWhatis <anton-gogo@mail.ru>
+ * @license  Unlicense <https://unlicense.org>
+ * @link     https://github.com/cashcarryshop/sizya
+ */
+
+namespace Tests\Traits;
+
+use CashCarryShop\Sizya\StocksGetterInterface;
+use CashCarryShop\Sizya\DTO\StockDTO;
+
+/**
+ * Трейт с тестами для получения остатков.
+ *
+ * @category TestTraits
+ * @package  Sizya
+ * @author   TheWhatis <anton-gogo@mail.ru>
+ * @license  Unlicense <https://unlicense.org>
+ * @link     https://github.com/cashcarryshop/sizya
+ *
+ * @see OrdersGetterByAdditionalInterface
+ */
+trait StocksGetterTests
+{
+    use CreateValidatorTrait;
+
+    public function testGetStocks(): void
+    {
+        $stocksGetter = $this->createStocksGetter();
+
+        if ($stocksGetter) {
+            $stocks = $stocksGetter->getStocks();
+
+            $this->assertContainsOnlyInstancesOf(StockDTO::class, $stocks);
+
+            $validator = $this->createValidator();
+            foreach ($stocks as $stock) {
+                $violations = $validator->validate($stock);
+                $this->assertCount(0, $violations);
+            }
+        }
+    }
+
+    abstract protected function createStocksGetter(): ?StocksGetterInterface;
+}
