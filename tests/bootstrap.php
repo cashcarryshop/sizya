5d7e73fcea9385b60376832375274c557795404c
TheWhatis <anton-gogo@mail.ru>
2024-09-27 06:49:01 +0700

    Удалил загрузку переменных окружения для тестов, также удалил dev зависимость

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,12 +12,3 @@
  */
 
 const ROOT = __DIR__;
-
-$repository = Dotenv\Repository\RepositoryBuilder::createWithNoAdapters()
-    ->addAdapter(Dotenv\Repository\Adapter\EnvConstAdapter::class)
-    ->addWriter(Dotenv\Repository\Adapter\PutenvAdapter::class)
-    ->immutable()
-    ->make();
-
-$dotenv = Dotenv\Dotenv::create($repository, __DIR__);
-$dotenv->load();
