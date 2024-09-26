fe55f2f0ef4e016f9eae73fd3d88886064766a6f
TheWhatis <anton-gogo@mail.ru>
2024-09-27 06:38:41 +0700

    Переписал трейт для работы с Ozon, в основном методы для генерации данных

--- tests/Traits/InteractsWithOzon.php
+++ tests/Traits/InteractsWithOzon.php
@@ -11,8 +11,9 @@
  * @link     https://github.com/cashcarryshop/sizya
  */
 
-namespace Tests\Traits;
+namespace CashCarryShop\Sizya\Tests\Traits;
 
+use Psr\Http\Message\ResponseInterface;
 use GuzzleHttp\Exception\RequestException;
 
 /**
@@ -28,85 +29,223 @@ use GuzzleHttp\Exception\RequestException;
  */
 trait InteractsWithOzon
 {
-    /**
-     * Данные авторизации
-     *
-     * @var array
-     */
-    protected static array $credentials = [];
+    use InteractsWithHttp;
 
     /**
-     * Настройка тестов МойСклад.
+     * Создать Response для получения товаров.
      *
-     * @return void
+     * @param array  $ids  Идентификаторы товаров
+     * @param ?array $skus SKU Товаров
+     *
+     * @return ResponseInterface[]
      */
-    public static function setUpBeforeClass(): void
+    protected static function makeProductsGetResponses(array $ids, ?array $skus = null): array
     {
-        $token    = \getenv('OZON_TOKEN');
-        $clientId = \getenv('OZON_CLIENT_ID');
-
-        if ($token && $clientId) {
-            static::$credentials = [
-                'token'    => $token,
-                'clientId' => (int) $clientId
-            ];
-        } else {
-            static::markTestSkipped('No credentials provided. Skipping tests');
+        if ($skus === null) {
+            $skus = \array_map(
+                static fn () => \random_int(100000000, 999999999),
+                $ids
+            );
         }
 
-        static::markSkippedIfBadResponse(
-            fn () => static::setUpBeforeClassByOzon(static::$credentials)
+        $template    = static::getResponseData("api-seller.ozon.ru/v2/product/list")['body'];
+        $templateRow = $template['result']['items'][0];
+
+        $template['result']['items'] = \array_map(
+            fn ($id, $sku) => static::makeListProduct([
+                'id'       => $id,
+                'sku'      => $sku,
+                'template' => $templateRow
+            ]),
+            $ids,
+            $skus
+        );
+
+        $template['result']['total'] = \count($template['result']['items']);
+
+        return \array_merge(
+            [static::createJsonResponse(body: $template)],
+            static::makeProductsGetByIdsResponses([$ids], [])
         );
     }
 
     /**
-     * Настройка тестов МойСклад с перехватом
-     * ошибки от api.
+     * Создать Response для запроса получения товаров
+     * по идентификаторам.
      *
-     * @param array $credentials Данные авторизации
+     * @param array $provides   Идентификаторы по чанкам
+     * @param array $invalidIds Неправильные идентификаторы
      *
-     * @return void
+     * @return ResponseInterface[]
      */
-    protected static function setUpBeforeClassByOzon(array $credentials): void
+    public static function makeProductsGetByIdsResponses(array $provides, array $invalidIds): array
     {
-        // ...
+        // Получение шабллонов
+        $template    = static::getResponseData("api-seller.ozon.ru/v2/product/info/list")['body'];
+        $templateRow = $template['result']['items'][0];
+
+        $makeRow = function ($id) use ($templateRow, $invalidIds) {
+            if (\in_array($id, $invalidIds)) {
+                return null;
+            }
+
+            return static::makeProduct([
+                'id'       => $id,
+                'template' => $templateRow
+            ]);
+        };
+
+        $makeResponse = function ($ids) use ($template, $makeRow) {
+            $template['result']['items'] = \array_filter(
+                \array_map($makeRow, $ids),
+                'is_array'
+            );
+
+            return static::createJsonResponse(body: $template);
+        };
+
+        return \array_map(fn ($ids) => $makeResponse($ids), $provides);
     }
 
     /**
-     * Сбросить данные
+     * Создать элемент метода получения unfulfilled отправлений.
      *
-     * @return void
+     * Массив $options принимает:
+     *
+     * - id:              (int)    Идентификатор заказа
+     * - article:         (string) Артикул товара
+     * - template:        (array)  Шаблон отправления
+     * - productId        (int)    Идентификатор товара
+     * - productArticle   (string) Артикул товара
+     * - productPrice     (int)    Цена товара
+     * - productQuantity  (int)    Количество товара
+     *
+     * @param array $options Опции
+     *
+     * @return array
      */
-    public static function tearDownAfterClass(): void
+    protected static function makePosting(array $options): array
     {
-        static::$credentials = [];
-        static::tearDownAfterClassByOzon();
-    }
-
-    /**
-     * Сбросить данные Ozon
-     *
-     * @return void
-     */
-    protected static function tearDownAfterClassByOzon(): void
-    {
-        // ...
-    }
-
-    /**
-     * Получить сущность через проверки
-     *
-     * @param callable(array):?object $resolve Получить данные авторизации
-     *                                         и вернуть сущность
-     *
-     * @return ?object
-     */
-    protected function getEntity(callable $resolve): ?object
-    {
-        return static::markSkippedIfBadResponse(
-            $resolve,
-            fn ($message) => $this->markTestSkipped($message)
+        $options = \array_replace(
+            [
+                'id'              => \random_int(100000000, 999999999),
+                'article'         => static::fakeArticle(),
+                'productSku'      => \random_int(100000000, 999999999),
+                'productArticle'  => static::fakeArticle(),
+                'productPrice'    => \random_int(100, 1000),
+                'productQuantity' => \random_int(1, 10)
+            ],
+            $options
         );
+
+        $options['template']['posting_number'] = (string) $options['id'];
+        $options['template']['article']        = $options['article'];
+
+        $options['template']['products'][0]['sku']      = $options['productSku'];
+        $options['template']['products'][0]['offer_id'] = $options['productArticle'];
+        $options['template']['products'][0]['price']    = (string) $options['productPrice'];
+        $options['template']['products'][0]['quantity'] = $options['productQuantity'];
+
+        return $options['template'];
+    }
+
+    /**
+     * Создать товар из product/list.
+     *
+     * Массив $options принимает:
+     *
+     * - id:      (int)    Идентификатор товара
+     * - article: (string) Артикул товара
+     *
+     * @param array $options Опции
+     *
+     * @return array
+     */
+    protected static function makeListProduct(array $options): array
+    {
+        $options = \array_replace(
+            [
+                'id'      => \random_int(100000000, 999999999),
+                'article' => static::fakeArticle()
+            ],
+            $options
+        );
+
+        $options['template']['product_id'] = $options['id'];
+        $options['template']['offer_id']   = $options['article'];
+
+        return $options['template'];
+    }
+
+    /**
+     * Создать товар из product/info/list
+     *
+     * Массив $options принимает:
+     *
+     * - id:      (int)    Идентификатор товара
+     * - sku:     (int)    Sku товара
+     * - article: (string) Артикул товара
+     * - price:   (float)  Цена
+     *
+     * @param array $options Опции
+     *
+     * @return array
+     */
+    protected static function makeProduct(array $options): array
+    {
+        $options = \array_replace(
+            [
+                'id'      => \random_int(100000000, 999999999),
+                'sku'     => \random_int(100000000, 999999999),
+                'article' => static::fakeArticle(),
+                'price'   => (float) \random_int(100, 1000)
+            ],
+            $options
+        );
+
+        $options['template']['id']       = $options['id'];
+        $options['template']['sku']      = $options['sku'];
+        $options['template']['offer_id'] = $options['article'];
+        $options['template']['price']    = (string) $options['price'];
+
+        return $options['template'];
+    }
+
+    /**
+     * Создать товар из product/info/list
+     *
+     * Массив $options принимает:
+     *
+     * - id:      (int)    Идентификатор товара
+     * - sku:     (int)    Sku товара
+     * - article: (string) Артикул товара
+     * - price:   (float)  Цена
+     *
+     * @param array $options Опции
+     *
+     * @return array
+     */
+    protected static function makeByWarehouseStock(array $options): array
+    {
+        $options = \array_replace(
+            [
+                'id'          => \random_int(100000000, 999999999),
+                'sku'         => \random_int(100000000, 999999999),
+                'quantity'    => \random_int(0, 10),
+                'reserved'    => \random_int(0, 10),
+                'warehouseId' => \random_int(100000000, 999999999)
+            ],
+            $options
+        );
+
+        $options['template']['product_id']   = $options['id'];
+        $options['template']['sku']          = $options['sku'];
+        $options['template']['fbs_sku']      = $options['sku'];
+        $options['template']['quantity']     = $options['quantity'];
+        $options['template']['reserved']     = $options['reserved'];
+        $options['template']['warehouse_id'] = $options['warehouseId'];
+
+        return $options['template'];
     }
 
     /**
